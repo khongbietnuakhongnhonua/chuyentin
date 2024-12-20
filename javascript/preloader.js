6b87a9c0f8ebed638ef4fd966aedadc0a28504a9
Ẩn danh <nguyenminhkhoi.nmk.mail@gmail.com>
2024-12-20 16:20:26 +0700

    thử nghiệm vài cái với fix bug

--- javascript/preloader.js
+++ javascript/preloader.js
@@ -1,26 +1,26 @@
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function() {
-    loader.classList.add('hidden');
+    loader.classList.add('hide');
 });
 
 var hidebody = document.getElementById("hidebody");
 window.addEventListener("load", function() {
-    hidebody.classList.add('hidden');
+    hidebody.classList.add('hide');
 });
 
 window.addEventListener("load", function() {
-    loader.classList.add('hidden');
+    loader.classList.add('hide');
 });
 
 window.addEventListener("load", function() {
     setTimeout(function() {
         hidebody.style.display = "none";
-    }, 1000);
+    }, 2000);
 });
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function() {
     setTimeout(function() {
         loader.style.display = "none";
-    }, 1000);
+    }, 2000);
 });
